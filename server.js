b670b389fcf033d3129c00e0a652debfcd62ad8a
Aayush Singh <aayushkt@gmail.com>
2023-04-26 18:54:19 -0400

    imported express

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import { rps, rpsls } from "./lib/rpsls.js"
 import minimist from "minimist"
+import express from "express"
 
 const cliArgs = minimist(process.argv.slice(2))
+const app = express()
 var port = 5000
 
 if(!(cliArgs.port == undefined)) {
     port = Number(cliArgs.port)
-    console.log(port)
     if (isNaN(port)) {
         port = 5000
     }
 }
 
-console.log(rps('paper'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
