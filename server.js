5fdca4b65743b5789ce9408f3979558b693432a8
Aayush Singh <aayushkt@gmail.com>
2023-04-26 18:49:50 -0400

    imported rpsls, made server.js a module

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+import { rps, rpsls } from "./lib/rpsls.js"
+import minimist from "minimist"
 
-const minimist = require("minimist")
 const cliArgs = minimist(process.argv.slice(2))
-var port = 5555
+var port = 5000
 
 if(!(cliArgs.port == undefined)) {
     port = Number(cliArgs.port)
     console.log(port)
     if (isNaN(port)) {
-        port = 5555
+        port = 5000
     }
 }
 
-console.log(port)
+console.log(rps('paper'))
